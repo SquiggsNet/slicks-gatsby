7602689b0203aff46253c7882cc2fd415f713289
squiggsnet <squiggs.rafael@gmail.com>
2021-04-11 21:27:05 -0300

    chore: refactor and clean up responsiveness

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -25,6 +25,13 @@ const PaginationStyles = styled.div`
       color: var(--grey);
     }
   }
+  @media (max-width: 800px) {
+    .word {
+      display: none;
+    }
+    font-size: 1.4rem;
+    
+  }
 `;
 
 export default function Pagination({
@@ -41,19 +48,20 @@ export default function Pagination({
   const hasPrevPage = prevPage >= 1;
   return (
     <PaginationStyles>
-      <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
-        &#8592; Prev
+      <Link title="prev page" disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
+        &#8592; <span className="word">Prev</span>
       </Link>
       {Array.from({ length: totalPages }).map((_, i) => (
         <Link
+          key={`paginate-${i}`}
           className={currentPage === 1 && i === 0 ? 'current' : ''}
           to={`${base}/${i > 0 ? i + 1 : ''}`}
         >
           {i + 1}
         </Link>
       ))}
-      <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
-        Next &#8594;
+      <Link title="next page" disabled={!hasNextPage} to={`${base}/${nextPage}`}>
+        <span className="word">Next</span> &#8594;
       </Link>
     </PaginationStyles>
   );
